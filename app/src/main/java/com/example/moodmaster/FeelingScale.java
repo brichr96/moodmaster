ef02f0692c34bb0ffaa723a0d40e6ed84bfab38b
p25b98 <christian.briegler@edu.fh-joanneum.at>
2023-05-24 14:39:38 +0200

    second

---
+++ app/src/main/java/com/example/moodmaster/FeelingScale.java
@@ -0,0 +1,14 @@
+package com.example.moodmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeelingScale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feelingscale);
+    }
+}
---
+++ app/src/main/java/com/example/moodmaster/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.moodmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+
+        final TextView moodMasterTextView = findViewById(R.id.textView);
+        final Handler handler = new Handler();
+
+        // Set up scale and fade in animation for Moodmaster text
+        Animation scaleAnimation = new ScaleAnimation(0.7f, 1.0f, 0.7f, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(3000);
+        scaleAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        moodMasterTextView.startAnimation(scaleAnimation);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, FeelingScale.class);
+                startActivity(intent);
+            }
+        }, 15000);  // 15000 milliseconds = 15 seconds
+    }
+}
